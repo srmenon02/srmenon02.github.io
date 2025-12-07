2eca4959fcded370f26e688cf1b6215b51f20c63
srmenon02 <smenon02@yahoo.com>
2025-12-07 09:28:46 -0800

    opacity typo

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import './App.css'
 import { LoadingScreen } from './components/LoadingScreen';
 import "./index.css";
 import { useState } from 'react';
+import { Navbar } from './components/Navbar';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}{" "}
+      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      </div>
     </>
   )
 }
