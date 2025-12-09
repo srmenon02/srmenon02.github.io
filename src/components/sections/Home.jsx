c16a4cf29aee06c56466e33d8fb145f7624d2348
srmenon02 <smenon02@yahoo.com>
2025-12-08 17:53:16 -0800

    Nav buttons

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -8,6 +8,25 @@ export const Home = () => {
             <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-tight">
                 Hi! I'm Suraj Menon
             </h1>
+
+            <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+                Software engineer passionate about the intersection of healthcare and tech.
+                I am fueled by opportunities to develop patient-focused solutions in healthcare.
+            </p>
+
+            <div className="flex justify-center space-x-4">
+                <a
+                href="#projects"
+                className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">
+                    View Projects
+                </a>
+                <a
+                href="#contact"
+                className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
+                            hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)] hover:bg-blue-500/10">
+                    Contact Me
+                </a>
+            </div>
         </div>
     </section>
     )
