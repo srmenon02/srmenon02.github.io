982bc3133a9ef3bdcb310191c6eda633c4f05250
srmenon02 <smenon02@yahoo.com>
2025-12-08 17:36:06 -0800

    Finished navigation bars

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { LoadingScreen } from './components/LoadingScreen';
 import "./index.css";
 import { useState } from 'react';
 import { Navbar } from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -12,6 +13,7 @@ function App() {
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}{" "}
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       </div>
     </>
   )
