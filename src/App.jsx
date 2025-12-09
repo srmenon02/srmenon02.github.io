b0c5b73853a76326d07b1194336729c1fb98874d
srmenon02 <smenon02@yahoo.com>
2025-12-08 17:44:12 -0800

    Added Home Component and header

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { useState } from 'react';
 import { Navbar } from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
+import { Home } from './components/sections/Home';
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -14,6 +15,7 @@ function App() {
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <Home />
       </div>
     </>
   )
---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+    return (
+    <section
+        id="home"
+        className="min-h-screen flex items-center justify-center relative"
+    >
+        <div className="text-center z-10 px-4">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-tight">
+                Hi! I'm Suraj Menon
+            </h1>
+        </div>
+    </section>
+    )
+}
