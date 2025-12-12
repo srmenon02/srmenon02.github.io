d3bb0543222c3d40d8c407dd748fbf9ce5c52a79
srmenon02 <smenon02@yahoo.com>
2025-12-12 13:33:40 -0700

    Updated base

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base: "/srmenon02.github.io/",  // ✅ Must match your repo name
 })
